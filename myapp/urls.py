39c0554a3a5a90717f8d9d68e3b135d2d2b85822
Bill Newman <william.newman@seattlecolleges.edu>
2024-06-05 01:59:20 +0000

    Adds Themes

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path("",views.home, name="home"),
+  path("about",views.about, name="about"),
+  path('function/', views.function_view, name='function_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
+]
